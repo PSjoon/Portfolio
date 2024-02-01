4084d004c0cbe662b3b06e518d1cd357aa97df9a
PSjoon <pedrosantos.joon@gmail.com>
2024-02-01 15:38:26 -0300

    add start main

---
+++ src/components/about/About.tsx
@@ -0,0 +1,14 @@
+import "./About.css"
+
+function About() {
+  return (
+    <>
+      <main>
+        <section className="hero"></section>
+        <section className="picture"></section>
+      </main>
+    </>
+  )
+}
+
+export default About
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,15 +4,54 @@ import { FaLinkedin } from "react-icons/fa"
 import { useState } from "react"
 import { FaGithub } from "react-icons/fa"
 
+import { IoClose } from "react-icons/io5"
+import { RxHamburgerMenu } from "react-icons/rx"
+
 function Header() {
   const [switchActivate, setSwitchActivate] = useState(false)
+
+  const [menuOpen, setMenuOpen] = useState(false)
 
   const tootleDarkTheme = () => {
     setSwitchActivate(!switchActivate)
   }
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
     <>
+      <div className="app">
+        <button className="menu-toggle" onClick={toggleMenu}>
+          {menuOpen ? <IoClose size={40} /> : <RxHamburgerMenu size={40} />}
+        </button>
+
+        <div className={`menu-container ${menuOpen ? "open" : ""}`}>
+          <p>Apresentação</p>
+          <p>Meus Projetos</p>
+          <p>Meu Currículo</p>
+          <p>Participações</p>
+        </div>
+
+        <div className="redes-mobile">
+          <a
+            href="https://www.linkedin.com/in/pedrosantosjoon/"
+            target="__blank"
+          >
+            <p> LinkedIn</p>
+            <FaLinkedin className="icone-redes" />
+          </a>
+
+          <a href="https://github.com/PSjoon" target="__blank">
+            <p>GitHub</p>
+            <FaGithub className="icone-redes" />
+          </a>
+
+          <Switch onChange={tootleDarkTheme} checked={switchActivate} />
+        </div>
+      </div>
+
       <header>
         <nav>
           <p>Apresentação</p>
